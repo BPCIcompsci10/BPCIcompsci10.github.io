f1463f0b02ec0e57164cb2d58236f0f08079426d
BPCIcompsci10 <sanjhana.routh@student.tdsb.on.ca>
2025-01-16 14:15:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -118,35 +118,28 @@ function checkPose(prediction, video) {
 
         switch(poseNumber) {
             case '1':
-                if (time >= 0.9 && time <= 3.0 && !poseState.triggered) {
+                if (time >= 0.7 && time <= 1.5 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '2':
-                if (time >= 5.5 && time <= 7.5 && !poseState.triggered) {
+                if (time >= 3.9 && time <= 4.9 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
+                 break;
             case '3':
-                if ((time >= 11.5 && time <= 13.0 && !poseState.firstWindowTriggered) ||
-                    (time >= 17.5 && time <= 19.5 && !poseState.secondWindowTriggered)) {
-                    if (time <= 13.0) {
-                        poseState.firstWindowTriggered = true;
-                    } else {
-                        poseState.secondWindowTriggered = true;
-                    }
-                    explosionActive = true;
-                    playExplosionSound();
-                    setTimeout(() => { explosionActive = false; }, 300);
+                if (time >= 6.9 && time <= 8.4 && !poseState.triggered) {
+                    triggerExplosion(poseState);
                 }
                 break;
             case '4':
-                if (time >= 15.5 && time <= 16.6 && !poseState.triggered) {
+                if (time >= 13.4 && time <= 14.4 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '5':
-                if (time >= 19.5 && !poseState.triggered) {
+                if (time >= 21.9 && time <= 22.9 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
