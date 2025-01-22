9ce527544b986d0aa276f48325e8f64ee6f0e73a
BPCIcompsci10 <sanjhana.routh@student.tdsb.on.ca>
2025-01-22 15:08:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function checkPose(prediction, video) {
         };
     }
 
-    if (prob > 0.7 && !explosionActive) {
+    if (prob > 0.6 && !explosionActive) {
         const poseState = poseStates[`pose${poseNumber}`];
 
         switch(poseNumber) {
