847d6d8fbab919b51bc5588d840a0add15c8f343
BPCIcompsci10 <sanjhana.routh@student.tdsb.on.ca>
2025-01-22 15:56:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/lzSIXo1jr/";
+let URL = "https://teachablemachine.withgoogle.com/models/KEuk8H4QD/";
 
 
 
@@ -224,7 +224,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/lzSIXo1jr/");
+        console.log("https://teachablemachine.withgoogle.com/models/KEuk8H4QD/");
     }
 }
 
